87de13c030b23310bd0b66b8c9ac70e1eef077b3
Tom Hindle <tom_hindle@sil.org>
2013-05-17 09:35:34 -0600

    Apply patch from Eberhard Beilharz - (slightly modified to build for .net 3.5)
    Instead of hardcoding the IBusEngineDesc struct we now build it
    dynamically based on the signature.
    
    This fixes the problem that each new version of ibus breaks
    ibusdotnet because of a modified IBusEngineDesc structure.

--- src/IBusEngineDesc.cs
+++ src/IBusEngineDesc.cs
@@ -7,168 +7,222 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+using NDesk.DBus;
 
 namespace IBusDotNet
 {
+	/// <summary>
+	/// Dynamically constructed IBusEngineDesc struct based on the dbus signature
+	/// </summary>
+	/// <remarks>Limitations: we follow the YAGNI approach and support currently only the basic
+	/// types. Also we assume that the order of properties of the same type stays the same (i.e.
+	/// that the second string property we get is always the longname). It would be better and
+	/// safer to get the IBusEngineDesc properties through the proper methods that ibus defines,
+	/// but that looked way more complicated and isn't currently needed. Besides, the ibus
+	/// code has a comment that the order of the serialized properties should not be changed
+	/// (https://github.com/ibus/ibus/blob/master/src/ibusenginedesc.c).</remarks>
 	public class IBusEngineDesc
 	{
-		// enums of supported ibus versions
-		// these numbers are NOT the same as the ibus version numbers.
-		private enum IBusVersions
+		private Type m_type;
+		private object m_engine;
+
+		private IBusEngineDesc(Type type, object engine)
 		{
-			Unknown,
-			Version1,
-			// commit e2793f52bf3da7a22321 changed IBusEngineDesc to contain rank field.
-			Version2,
-			Version_1_3_7,
-			Version_1_3_99,
-			Version_1_4,
-			Version_1_4_1,
+			m_type = type;
+			m_engine = Convert.ChangeType(engine, type);
 		}
 
-		private static IBusVersions s_ibusVersion = IBusVersions.Unknown;
-
-		private IBusEngineDesc()
+		// copied (and slightly modified) from https://github.com/mono/dbus-sharp/blob/master/src/DType.cs
+		private enum DType
 		{
+			Invalid = '\0',
+
+			Byte = 'y',
+			Boolean = 'b',
+			Int16 = 'n',
+			UInt16 = 'q',
+			Int32 = 'i',
+			UInt32 = 'u',
+			Int64 = 'x',
+			UInt64 = 't',
+			Single = 'f', //This is not yet supported!
+			Double = 'd',
+			String = 's',
+			ObjectPath = 'o',
+			Signature = 'g',
+
+			Array = 'a',
+			[Obsolete ("Not in protocol")]
+			Struct = 'r',
+			[Obsolete ("Not in protocol")]
+			DictEntry = 'e',
+			Variant = 'v',
+
+			StructBegin = '(',
+			StructEnd = ')',
+			DictEntryBegin = '{',
+			DictEntryEnd = '}',
 		}
 
-		public string Name { get; private set; }
-		public string LongName { get; private set; }
-		public string Description { get; private set; }
-		public string Language { get; private set; }
-		public string License { get; private set; }
-		public string Author { get; private set; }
-		public string Icon { get; private set; }
-		public string Layout { get; private set; }
-		public string Hotkeys { get; private set; }
-		public UInt32 Rank { get; private set; }
-		public string Symbol { get; private set; }
-		public string Setup { get; private set; }
+		/// <summary>
+		/// The field names we know (see https://github.com/ibus/ibus/blob/master/src/ibusenginedesc.c).
+		/// </summary>
+		private readonly static string[] StringFieldNames = { "a", "longname", "name",
+			"description", "language", "license", "author", "icon", "layout",
+			"hotkeys", "symbol", "setup", "layoutVariant", "layoutOption", "version",
+			"textDomain"};
+		private readonly static string[] IntFieldNames = { "rank" };
 
-		private static T? ConvertToEngineDesc<T>(object engine)
+		private static Type GetTypeFromDType(string signature, out int advanceChars)
 		{
-			try
+			advanceChars = 0;
+			for (int i = 0; i < signature.Length; i++)
 			{
-				System.Diagnostics.Debug.WriteLine(string.Format("Trying to convert '{0}' to {1}",
-					engine, typeof(T)));
-				return (T)Convert.ChangeType(engine, typeof(T));
+				advanceChars++;
+				switch ((DType)signature[i])
+				{
+					case DType.Array: // 'a'
+						// should be something like a{sv}
+						if (signature.Length < 5 || signature[1] != '{' || signature[4] != '}')
+						{
+							advanceChars = 0;
+							return null;
+						}
+
+						// see http://stackoverflow.com/a/1151470 on how to dynamically create a
+						// generic type
+						int advanceChars1, advanceChars2;
+						var dictionaryType = typeof(IDictionary<,>);
+						var genericType = dictionaryType.MakeGenericType(
+							GetTypeFromDType(signature.Substring(2), out advanceChars1),
+							GetTypeFromDType(signature.Substring(3), out advanceChars2));
+						advanceChars = 3 + advanceChars1 + advanceChars2;
+						return genericType;
+					case DType.String: // 's'
+						return typeof(string);
+					case DType.Byte: // 'y'
+						return typeof(Byte);
+					case DType.Boolean: // 'b'
+						return typeof(bool);
+					case DType.Int16: // 'n'
+						return typeof(Int16);
+					case DType.UInt16: // 'q'
+						return typeof(UInt16);
+					case DType.Int32: // 'i'
+						return typeof(Int32);
+					case DType.UInt32: // 'u'
+						return typeof(UInt32);
+					case DType.Int64: // 'x'
+						return typeof(Int64);
+					case DType.UInt64: // 't'
+						return typeof(UInt64);
+					case DType.Single: // 'f'
+						return typeof(Single);
+					case DType.Double: // 'd'
+						return typeof(Double);
+					case DType.ObjectPath: // 'o'
+						return typeof(ObjectPath);
+					case DType.Signature: // 'g'
+						return typeof(string);
+					case DType.Variant: // 'v'
+						return typeof(object);
+					case DType.StructBegin:
+					case DType.StructEnd:
+						continue;
+					default:
+						advanceChars = 0;
+						return null;
+				}
 			}
-			catch (InvalidCastException)
-			{
-				return null;
-			}
+			return null;
 		}
+
+		/// <summary>
+		/// Create an IBus Engine Description based on the DBus signature
+		/// </summary>
+		/// <returns>The ibus engine description struct.</returns>
+		/// <param name="signature">DBus type signature.</param>
+		internal static Type CreateEngineDesc(string signature)
+		{
+			var assemblyName = new AssemblyName();
+			assemblyName.Name = "IBusEngineDescAssembly";
+
+			var domain = Thread.GetDomain();
+			var assemblyBuilder = domain.DefineDynamicAssembly(assemblyName,
+				AssemblyBuilderAccess.Run);
+			var moduleBuilder = assemblyBuilder.DefineDynamicModule("IBusEngineDescModule");
+
+			var typeBuilder = moduleBuilder.DefineType("IBusEngineDesc");
+
+			int fieldCount = 0;
+			int stringFieldCount = 0;
+			int intFieldCount = 0;
+			for (int i = 0; i < signature.Length; fieldCount++)
+			{
+				var fieldName = string.Format("field_{0}", fieldCount);
+				int advanceChars;
+				Type t = GetTypeFromDType(signature.Substring(i), out advanceChars);
+				if (t == null && advanceChars == 0)
+				{
+					throw new NotSupportedException(string.Format("Unknown ibus version with signature {0}.",
+						signature));
+				}
+
+				i += advanceChars;
+				if (t == null)
+					continue;
+
+				switch ((DType)signature[i - 1])
+				{
+					case DType.String: // 's'
+						if (stringFieldCount < StringFieldNames.Length)
+							fieldName = StringFieldNames[stringFieldCount++];
+						break;
+					case DType.UInt32: // 'u'
+						if (intFieldCount < IntFieldNames.Length)
+							fieldName = IntFieldNames[intFieldCount++];
+						break;
+					default:
+						break;
+				}
+				typeBuilder.DefineField(fieldName, t, FieldAttributes.Public);
+			}
+			return typeBuilder.CreateType();
+		}
+
+		private T GetProperty<T>(string fieldName)
+		{
+			var field = m_type.GetField(fieldName);
+			if (field != null)
+				return (T)field.GetValue(m_engine);
+			return default(T);
+		}
+
+		public string Name { get { return GetProperty<string>("name"); } }
+		public string LongName { get { return GetProperty<string>("longname"); } }
+		public string Description { get { return GetProperty<string>("description"); } }
+		public string Language { get { return GetProperty<string>("language"); } }
+		public string License { get { return GetProperty<string>("license"); } }
+		public string Author { get { return GetProperty<string>("author"); } }
+		public string Icon { get { return GetProperty<string>("icon"); } }
+		public string Layout { get { return GetProperty<string>("layout"); } }
+		public string Hotkeys { get { return GetProperty<string>("hotkeys"); } }
+		public UInt32 Rank { get { return GetProperty<UInt32>("rank"); } }
+		public string Symbol { get { return GetProperty<string>("symbol"); } }
+		public string Setup { get { return GetProperty<string>("setup"); } }
+		public string LayoutVariant { get { return GetProperty<string>("layoutVariant"); } }
+		public string LayoutOption { get { return GetProperty<string>("layoutOption"); } }
+		public string Version { get { return GetProperty<string>("version"); } }
+		public string TextDomain { get { return GetProperty<string>("textDomain"); } }
 
 		/// <summary>Get IBusEngineDesc names in a way tolerant to IBusEngineDesc versions.</summary>
 		public static IBusEngineDesc GetEngineDesc(object engine)
 		{
-			switch (s_ibusVersion)
-			{
-				case IBusVersions.Unknown:
-				case IBusVersions.Version1:
-				{
-					var engineDesc = ConvertToEngineDesc<IBusEngineDesc_v1>(engine);
-					if (engineDesc.HasValue)
-					{
-						return new IBusEngineDesc { LongName = engineDesc.Value.longname,
-							Name = engineDesc.Value.name, Description = engineDesc.Value.description,
-							Language = engineDesc.Value.language, License = engineDesc.Value.license,
-							Author = engineDesc.Value.author, Icon = engineDesc.Value.icon,
-							Layout = engineDesc.Value.layout };
-					}
-					// try next version.
-					s_ibusVersion = IBusVersions.Version2;
-					break;
-				}
-
-				case IBusVersions.Version2:
-				{
-					var engineDesc = ConvertToEngineDesc<IBusEngineDesc_v2>(engine);
-					if (engineDesc.HasValue)
-					{
-						return new IBusEngineDesc { LongName = engineDesc.Value.longname,
-							Name = engineDesc.Value.name, Description = engineDesc.Value.description,
-							Language = engineDesc.Value.language, License = engineDesc.Value.license,
-							Author = engineDesc.Value.author, Icon = engineDesc.Value.icon,
-							Layout = engineDesc.Value.layout, Rank = engineDesc.Value.rank };
-					}
-					// try next version.
-					s_ibusVersion = IBusVersions.Version_1_3_7;
-					break;
-				}
-
-				case IBusVersions.Version_1_3_7:
-				{
-					var engineDesc = ConvertToEngineDesc<IBusEngineDesc_v3>(engine);
-					if (engineDesc.HasValue)
-					{
-						return new IBusEngineDesc { LongName = engineDesc.Value.longname,
-							Name = engineDesc.Value.name, Description = engineDesc.Value.description,
-							Language = engineDesc.Value.language, License = engineDesc.Value.license,
-							Author = engineDesc.Value.author, Icon = engineDesc.Value.icon,
-							Layout = engineDesc.Value.layout, Rank = engineDesc.Value.rank,
-							Hotkeys = engineDesc.Value.hotkeys };
-					}
-					// try next version.
-					s_ibusVersion = IBusVersions.Version_1_3_99;
-					break;
-				}
-
-				case IBusVersions.Version_1_3_99:
-					{
-						var engineDesc = ConvertToEngineDesc<IBusEngineDesc_v4>(engine);
-						if (engineDesc.HasValue)
-						{
-							return new IBusEngineDesc { LongName = engineDesc.Value.longname,
-								Name = engineDesc.Value.name, Description = engineDesc.Value.description,
-								Language = engineDesc.Value.language, License = engineDesc.Value.license,
-								Author = engineDesc.Value.author, Icon = engineDesc.Value.icon,
-								Layout = engineDesc.Value.layout, Rank = engineDesc.Value.rank,
-								Hotkeys = engineDesc.Value.hotkeys };
-						}
-						// try next version.
-						s_ibusVersion = IBusVersions.Version_1_4;
-						break;
-					}
-
-				case IBusVersions.Version_1_4:
-				{
-					var engineDesc = ConvertToEngineDesc<IBusEngineDesc_v5>(engine);
-					if (engineDesc.HasValue)
-					{
-						return new IBusEngineDesc { LongName = engineDesc.Value.longname,
-							Name = engineDesc.Value.name, Description = engineDesc.Value.description,
-							Language = engineDesc.Value.language, License = engineDesc.Value.license,
-							Author = engineDesc.Value.author, Icon = engineDesc.Value.icon,
-							Layout = engineDesc.Value.layout, Rank = engineDesc.Value.rank,
-							Hotkeys = engineDesc.Value.hotkeys, Symbol = engineDesc.Value.symbol };
-					}
-					// try next version.
-					s_ibusVersion = IBusVersions.Version_1_4_1;
-					break;
-				}
-
-				default:
-				case IBusVersions.Version_1_4_1:
-				{
-					var engineDesc = ConvertToEngineDesc<IBusEngineDesc_v6>(engine);
-					if (engineDesc.HasValue)
-					{
-						return new IBusEngineDesc { LongName = engineDesc.Value.longname,
-							Name = engineDesc.Value.name, Description = engineDesc.Value.description,
-							Language = engineDesc.Value.language, License = engineDesc.Value.license,
-							Author = engineDesc.Value.author, Icon = engineDesc.Value.icon,
-							Layout = engineDesc.Value.layout, Rank = engineDesc.Value.rank,
-							Hotkeys = engineDesc.Value.hotkeys, Symbol = engineDesc.Value.symbol,
-							Setup = engineDesc.Value.setup };
-					}
-
-					Console.WriteLine("*** Unknown ibus version");
-					throw new NotSupportedException("Unknown ibus version with signature " +
-						engine.ToString());
-				}
-			}
-			return GetEngineDesc(engine);
+			return new IBusEngineDesc(CreateEngineDesc(engine.ToString()), engine);
 		}
 	}
 }
