b5cdf4f584f5f37b9b3bd6e4d5532d7973a7ddb4
Eberhard Beilharz <eb1@sil.org>
2015-03-04 16:16:41 +0100

    Remove debug code

--- src/InputContext.cs
+++ src/InputContext.cs
@@ -207,7 +207,6 @@ namespace IBusDotNet
 		public bool IsEnabled()
 		{
 			// IsEnabled is no longer available in IBus 1.5
-			var x = m_inputContext.Introspect();
 			if (m_inputContext.Introspect().Contains("<method name=\"IsEnabled\""))
 				return m_inputContext.IsEnabled();
 			return true;
